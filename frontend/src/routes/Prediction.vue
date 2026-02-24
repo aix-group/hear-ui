993da63055e68c9ba103d36354568139019a5dce
Adelia Manafov <manafov@students.uni-marburg.de>
2026-02-24 17:13:20 +0100

    i18n(prediction): add Reset all translations (en,de); style reset-all as error

--- frontend/src/routes/Prediction.vue
+++ frontend/src/routes/Prediction.vue
@@ -278,7 +278,7 @@
               </div>
               <!-- Reset all overrides button (bottom-right) -->
               <div class="whatif-reset-all">
-                <v-btn small color="secondary" variant="tonal" @click="clearAllWhatIf">
+                <v-btn small color="error" variant="tonal" @click="clearAllWhatIf">
                   <v-icon left>mdi-backspace-outline</v-icon>
                   {{ $t('prediction.whatif.reset_all') }}
                 </v-btn>
